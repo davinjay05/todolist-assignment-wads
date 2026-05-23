16020cb8ef49f7b2be1c201848e60bd7d8d7b12d
Davin Alexander <davinalexander708@gmail.com>
2026-05-23 20:40:45 +0700

    Jest Fixed

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
-import nextJest from 'next/jest.js'
+const nextJest = require('next/jest.js')
 
 const createJestConfig = nextJest({
   dir: './',
@@ -13,4 +13,4 @@ const customJestConfig = {
   },
 }
 
-export default createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig)
